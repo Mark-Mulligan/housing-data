c2856e3cbcd75203ae74630e76b0d84368e42e67
Mark-Mulligan <mark.mulligan.jr1@gmail.com>
2022-10-31 10:36:49 -0500

    updated navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,9 +1,17 @@
 // Next
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+const activeNavItemCSS =
+  "rounded-md bg-gray-900 px-3 py-2 font-medium text-white";
+const nonActiveNavItemCSS =
+  "rounded-md px-3 py-2 font-medium text-gray-300 hover:bg-gray-800 hover:text-white";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <nav className="border-gray-200 bg-gray-700 px-2 py-2.5 sm:px-4">
+    <nav className="border-gray-200 bg-gray-700 px-2 py-2 sm:px-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         <a href="https://flowbite.com/" className="flex items-center">
           <img
@@ -41,14 +49,26 @@ const Navbar = () => {
           <ul className="mt-4 flex flex-col p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:font-medium">
             <li>
               <Link href="/" passHref={true}>
-                <a className="block rounded py-2 pr-4 pl-3 text-gray-300 md:bg-transparent md:p-0">
+                <a
+                  className={
+                    router?.pathname === "/"
+                      ? activeNavItemCSS
+                      : nonActiveNavItemCSS
+                  }
+                >
                   National
                 </a>
               </Link>
             </li>
             <li>
               <Link href="/state" passHref={true}>
-                <a className="block rounded py-2 pr-4 pl-3 text-gray-300 md:bg-transparent md:p-0">
+                <a
+                  className={
+                    router?.pathname === "/state"
+                      ? activeNavItemCSS
+                      : nonActiveNavItemCSS
+                  }
+                >
                   State
                 </a>
               </Link>
