ea332047455754682a04f2d49985bc194ecaa3c2
Mark-Mulligan <mark.mulligan.jr1@gmail.com>
2022-11-11 11:02:41 -0600

    made state page and map more responsive

--- src/pages/state/index.tsx
+++ src/pages/state/index.tsx
@@ -77,15 +77,15 @@ const StatePage = () => {
       </Head>
       <div className="px-2 sm:px-4">
         <main className="container relative mx-auto pb-4">
-          <div className="absolute mt-24 w-full text-center">
+          <div className="mt-16 w-full text-center md:absolute md:mt-24">
             <h2 className="mb-4 text-center text-3xl text-white">
               Monthly Inventory Data By State
             </h2>
-            <p className="m-auto mb-6 max-w-xl text-center text-slate-400">
+            <p className="m-auto max-w-xl text-center text-slate-400">
               Click on a state in the map to view that state&apos;s data below.
             </p>
           </div>
-          <div className="h-32" />
+          <div className="h-0 md:h-32" />
 
           <div className="m-auto mb-4 max-w-[850px]">
             <USMap />
